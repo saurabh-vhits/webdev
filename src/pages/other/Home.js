ed61c64f40fe67e23b5b15f45ce766ca3cd031af
saurabh-vhits <saurabhsoni.vhits@gmail.com>
2023-11-07 17:51:53 +0530

    wd-01 initial commit

--- src/pages/other/Home.js
+++ src/pages/other/Home.js
@@ -2,7 +2,11 @@ import React from "react";
 import Index from "../../components/MuiIndex";
 
 const Home = () => {
-  return <Index.Box></Index.Box>;
+  return (
+    <Index.Box>
+      <h1>Home Page</h1>
+    </Index.Box>
+  );
 };
 
 export default Home;
